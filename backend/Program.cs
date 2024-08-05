732150f3673464a44bb8066bd4d3adf60e572f00
AbdoulWadoudou <85686951+AbdoulWadoudou@users.noreply.github.com>
2024-08-05 14:54:52 +0000

    crud users

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,6 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using Backend.Data;
+using Backend.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// Ajoutez cette ligne pour configurer HTTPS
+builder.Services.AddHttpsRedirection(options =>
+{
+    options.HttpsPort = 5001; // Ou le port que vous souhaitez utiliser pour HTTPS
+});
+
+// Ajouter la configuration du DbContext
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Ajouter le service UserService
+builder.Services.AddScoped<UserService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
