2f00167bc81aceb5a209ddbe01dfdcdb5ce28905
ImeneLEG <imene.legsir@gmail.com>
2024-08-09 20:10:57 +0100

    final archi

--- backend/obj/Debug/net8.0/Projet_Sqli.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/Projet_Sqli.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projet_Sqli")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e52d1369ba847f1c29436c7e7f8654157959e67b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3825789f0b8626f72d0aca3578b96b2ca5fbf86a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projet_Sqli")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projet_Sqli")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
