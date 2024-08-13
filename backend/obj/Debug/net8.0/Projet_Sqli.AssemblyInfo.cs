50cd2d7511b99bef7f33705cd00f7fb454b55537
ImeneLEG <imene.legsir@gmail.com>
2024-08-13 15:02:52 +0100

    commits before merge

--- backend/obj/Debug/net8.0/Projet_Sqli.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/Projet_Sqli.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projet_Sqli")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9742b0cafbd2eb7568b843c56bcd2b96486faafe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3582b1d73dc725cc01fb3edd96dd36b9f883e9b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projet_Sqli")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projet_Sqli")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- backend/obj/Release/net8.0/Projet_Sqli.AssemblyInfo.cs
+++ backend/obj/Release/net8.0/Projet_Sqli.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projet_Sqli")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b668528ece492af3e920f14cfb4e9fb14a1f3944")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3582b1d73dc725cc01fb3edd96dd36b9f883e9b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projet_Sqli")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projet_Sqli")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
