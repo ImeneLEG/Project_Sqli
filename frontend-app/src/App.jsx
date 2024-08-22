83c355d54f6e1e774956f082d1b963fbbb2eb729
nadia-azam <azam.nadia.2002@gmail.com>
2024-08-22 18:21:46 +0100

    changment de app.jsx routers

--- frontend-app/src/App.jsx
+++ frontend-app/src/App.jsx
@@ -1,22 +1,43 @@
 import React from 'react'
-import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import { BrowserRouter,Routes,Route , useLocation  } from 'react-router-dom'
 import {Box} from '@mui/material'
 
 import {Navbar,Feed,SearchFeed,VideoDetail} from './UserPart/components';
-const App = () => {
+import Login from './Authentification/Login/Login';
+import SignUp from './Authentification/SignUp/SignUp';
+
+
+const AppWrapper = () => {
+  const location = useLocation();
+
+  // Check if the current path is login or signUp
+  const shouldShowLayout = location.pathname !== '/login' && location.pathname !== '/signup';
+
   return (
-    <BrowserRouter>
-    <Box sx={{backgroundColor:'#000'}}>
-    <Navbar/>
-    <Routes>
-    <Route path='/' exact element={<Feed/>}/>
-    <Route path='/video/:id' exact element={<VideoDetail/>}/>
-    <Route path='/search/:searchTerm' exact element={<SearchFeed/>}/>
+    <>
+      {shouldShowLayout ? (
+        <Box sx={{ backgroundColor: '#000' }}>
+          <Navbar />
+          <Routes>
+            <Route path='/' exact element={<Feed />} />
+            <Route path='/video/:id' exact element={<VideoDetail />} />
+            <Route path='/search/:searchTerm' exact element={<SearchFeed />} />
+          </Routes>
+        </Box>
+      ) : (
+        <Routes>
+          <Route path='/login' exact element={<Login />} />
+          <Route path='/signup' exact element={<SignUp />} />
+        </Routes>
+      )}
+    </>
+  );
+};
 
-    </Routes>
-    </Box>
-    </BrowserRouter>
-  )
-}
+const App = () => (
+  <BrowserRouter>
+    <AppWrapper />
+  </BrowserRouter>
+);
 
-export default App
+export default App;
