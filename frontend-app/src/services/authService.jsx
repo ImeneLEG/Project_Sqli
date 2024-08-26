df9720bd1534b82b4c79e82031db27952553329d
ImeneLEG <imene.legsir@gmail.com>
2024-08-26 18:32:18 +0100

    pull last version

--- frontend-app/src/services/authService.jsx
+++ frontend-app/src/services/authService.jsx
@@ -4,7 +4,7 @@ const API_URL = 'https://localhost:7275/api/Auth/';
 
 export const signUp = async (userData) => {
     try {
-        const response = await axios.post(`${API_URL}register`, userData);
+        const response = await axios.post(`${API_URL}register`, userData );
         return {
             status: response.status,
             data: response.data
@@ -19,7 +19,7 @@ export const signUp = async (userData) => {
 
 export const login = async (userData) => {
     try {
-        const response = await axios.post(`${API_URL}login`, userData);
+        const response = await axios.post(`${API_URL}login`, userData , { withCredentials: true });
         return {
             status: response.status,
             data: response.data
